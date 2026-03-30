18c7f37f4faa6c1f5be9ac8a47c175d3ad5683bf
kgowthami33 <kgowthami33@gmail.com>
2026-03-30 13:49:56 +0000

    Testing Build Trigger

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello DevOps Fans."}
-    return render_template('index.html', model=model)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    model = {"title":  "Hello Build Trigger."}
+    return render_template("index.html", model=model)
