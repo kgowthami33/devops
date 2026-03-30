24cabe931fc6fc341a5f1d5474096a7aa28c3cfb
kgowthami33 <kgowthami33@gmail.com>
2026-03-30 14:01:35 +0000

    Testing Build Trigger

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title":  "Hello Build Trigger."}
+    model = {"title":  "Hello Build Trigger2."}
     return render_template("index.html", model=model)
